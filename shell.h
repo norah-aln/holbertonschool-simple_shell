8abb5df47a41971ef18613076dff514a40de3dca
norah-aln <zylayo@gmail.com>
2025-11-05 14:08:53 +0000

    Task 1: Add Betty-compliant helper functions

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+/* String functions */
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+
+#endif
